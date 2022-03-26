d6c295a25bdf286e26c9bd7d2d440a5242d281bc
ZaKaeDa <l2jerangon@gmail.com>
2022-03-25 21:26:57 -0400

    first commit Guitarla NextJs

---
+++ components/Layout.js
@@ -0,0 +1,18 @@
+import Head from "next/head"
+
+const Layout = ({children, pagina}) => {
+  return (
+    <div>
+
+        <Head>
+            <title>GuitarLA - {pagina}</title>
+            <meta name="description" content="Sitio web de venta de guitarras" />
+        </Head>
+        <h1>Desde Layout</h1>
+
+        {children}
+    </div>
+  )
+}
+
+export default Layout
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+export default function Home() {
+    return (
+        <Layout
+            pagina="Inicio"
+        >
+            <h1>Desde Home</h1>
+        </Layout>
+    );
+}
